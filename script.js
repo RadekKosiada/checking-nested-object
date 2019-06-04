368721595e03a41c500f083d3060415d6611f24c
RadekKosiada <kosiadar@gmail.com>
2019-06-04 15:48:43 +0200

    Add for in loop

--- script.js
+++ script.js
@@ -16,24 +16,43 @@ var smallObj = {
     'main-dish': 0,
     'others': 0
 }
-// I built a UI in Vue.js that updates the booleans in real time 
-// and with every change I check for the true/false with this for loop
-// The problem: if I update the booleans from the bottom or middle the smallObj is not updated;
-// Only if I start from the first key: lasagna the smallObj is being updated;
 
-for(var i = 0; i < Object.keys(bigObj).length; i ++) {
-    if(Object.keys(smallObj)[i] === Object.keys(bigObj)[i]) {
-        for(var j =0; j < Object.keys(bigObj[Object.keys(bigObj)[i]]).length; j++) {
 
-            if(!Object.values(bigObj[Object.keys(bigObj)[i]])[j]) {
-                    smallObj[Object.keys(bigObj)[i]]++
-                    console.log(Object.keys(bigObj[Object.keys(bigObj)[i]])[j]);
-            } else {
-                console.log(Object.keys(bigObj[Object.keys(bigObj)[i]])[j]);
-            }
-        }
+  
+  for (const key in bigObj) {
+    let value = bigObj[key];
+    
+    //optional check for properties from prototype chain
+    if (bigObj.hasOwnProperty(key)) {
+      //no a property from prototype chain  
+      console.log(key)   
+      if(!bigObj[key])
+      smallObj[key]++;
+    }else{
+      //property from protytpe chain
+      console.log(bigObj[key])
     }
-}
+  }
+
+
+// // I built a UI in Vue.js that updates the booleans in real time 
+// // and with every change I check for the true/false with this for loop
+// // The problem: if I update the booleans from the bottom or middle the smallObj is not updated;
+// // Only if I start from the first key: lasagna the smallObj is being updated;
+
+// for(var i = 0; i < Object.keys(bigObj).length; i ++) {
+//     if(Object.keys(smallObj)[i] === Object.keys(bigObj)[i]) {
+//         for(var j =0; j < Object.keys(bigObj[Object.keys(bigObj)[i]]).length; j++) {
+
+//             if(!Object.values(bigObj[Object.keys(bigObj)[i]])[j]) {
+//                     smallObj[Object.keys(bigObj)[i]]++
+//                     console.log(Object.keys(bigObj[Object.keys(bigObj)[i]])[j]);
+//             } else {
+//                 console.log(Object.keys(bigObj[Object.keys(bigObj)[i]])[j]);
+//             }
+//         }
+//     }
+// }
 
 console.log(smallObj)
 
